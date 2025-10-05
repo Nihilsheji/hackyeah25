35dbc84d2018f333dadda5da924f64c87d185758
Tomasz Marek <42216440+Llysei@users.noreply.github.com>
2025-10-05 09:46:42 +0200

    Add attack sounds for lust and glucio

--- SayMyName/Assets/Enemies/DamagerCollider.cs
+++ SayMyName/Assets/Enemies/DamagerCollider.cs
@@ -7,11 +7,13 @@ public class DamagerCollider : MonoBehaviour
 {
     [SerializeField] private IntEvent damagePlayerEvent;
     [SerializeField] private int damageAmount;
+    [SerializeField] private AudioSource attackAudioSource;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            if(attackAudioSource != null) {  attackAudioSource.Play(); }
             damagePlayerEvent.Raise(damageAmount);
         }
     }
